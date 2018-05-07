9ea3f1cfa60091776740eebda6120bb9a8c7b11f
DarkHaliax96 <angel.r.ruiz.m@gmail.com>
2018-05-06 22:44:17 -0500

    Commented 404

--- server.js
+++ server.js
@@ -25,8 +25,8 @@ app.use(function(req, res, next) {
 var routes = require('./api/routes/routes');
 routes(app);
 
-app.use(function(req, res) {
+/*app.use(function(req, res) {
     res.status(404).send({url: req.originalUrl + ' not found'})
-  });
+  });*/
 
 app.listen(port);
